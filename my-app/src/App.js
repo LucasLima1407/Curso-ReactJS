310de0cd08b370a5e7b987622217c10d294edc74
Ant. Lucas Vieira de Lima <lucaslima14072003@gmail.com>
2023-02-01 13:37:36 -0300

    Aula07
    Utilização de fragments para um código mais limpo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
-import Frase from './components/Frase'
+import Frase from './components/Frase';
+import List from './components/List';
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
       <SayMyName nome='Lucas' />
       <SayMyName nome={nome} />
       <Pessoa nome='Lucas' idade='20' profissao='Programador' foto='https://via.placeholder/150'/>
+      <List  />
     </div>
   );
 }
---
+++ my-app/src/components/List.js
@@ -0,0 +1,12 @@
+import Item from './Item'
+function List(){
+    return(
+        <>
+            <ul>
+                <Item marca='Ferrari'/>
+                <Item marca='Mercedes'/>
+            </ul>
+        </>
+    )
+}
+export default List
