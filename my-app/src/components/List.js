c1b46a794a1b8be9be0f1e3c0fbe167a5bd01fca
Ant. Lucas Vieira de Lima <lucaslima14072003@gmail.com>
2023-02-01 14:51:13 -0300

    Aula08
    avançando no uso de props e aprendendo a utilizar propTypes

--- my-app/src/components/List.js
+++ my-app/src/components/List.js
@@ -3,8 +3,9 @@ function List(){
     return(
         <>
             <ul>
-                <Item marca='Ferrari'/>
-                <Item marca='Mercedes'/>
+                <Item marca='Ferrari' ano_lancamento={2012}/>
+                <Item marca='Mercedes' ano_lancamento={2022}/>
+                <Item />
             </ul>
         </>
     )
